da6bdcf4893572a8708fcd8fea0f5b66eb9998e7
aelris <aelrismira@gmail.com>
2019-02-08 18:52:44 +0200

    future completed

--- src/main/java/com/epam/streaming/KafkaProd.scala
+++ src/main/java/com/epam/streaming/KafkaProd.scala
@@ -1,11 +1,12 @@
 package com.epam.streaming
 
-import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
+import org.apache.kafka.clients.producer.ProducerRecord
 
 import scala.concurrent.Future
 import scala.io.BufferedSource
 
 object KafkaProd extends App {
+  println(s"Start app")
   implicit val executor = scala.concurrent.ExecutionContext.global
   val topic = util.Try(args(0)).getOrElse("StreamingTopic")
   println(s"Connecting to $topic")
@@ -20,12 +21,14 @@ object KafkaProd extends App {
     .getLines
     .map { line =>
       Future {
-        val producer: KafkaProducer[Integer, String] = KafkaConf.getProducer
+        val producer = KafkaConf.getProducer
         val p: ProducerRecord[Integer, String] = new ProducerRecord(topic, 1, line)
         producer.send(p)
       }
     }.toList
 
+  println(s"before sleap")
   Thread.sleep(60000)
+  println(s"End app")
 
 }
